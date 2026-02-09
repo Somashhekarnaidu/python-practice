53ae62b60a52ab418397a81e1e9feb4b38ef4a38
Somashhekarnaidu <192472347.simats@saveetha.com>
2026-02-09 22:18:35 +0530

    Add files via upload

---
+++ Student pass or fail calculation.py
@@ -0,0 +1,6 @@
+student=input("Enterthe name of student:")
+marks=int(input("Enter the marks:"))
+if marks>=50:
+    print(student,"has passed","marks is",marks)
+else:
+    print(student,"has failed","marks is",marks)
